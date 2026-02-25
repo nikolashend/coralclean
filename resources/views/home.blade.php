9aabdb25514c9b3983c014819bbfe4e1e270a7d8
vadim.markin <vadim.markin.1979.05@gmail.com>
2026-02-25 21:40:09 +0200

    adde gtag manager

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ $locale ?? 'ru' }}">
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TLBQVF6P');</script>
+    <!-- End Google Tag Manager -->
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ __('home.meta_title') }}</title>
@@ -184,6 +192,11 @@
     <script type="application/ld+json">{!! $schemaReviews !!}</script>
 </head>
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TLBQVF6P"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    
     <!-- Preloader -->
     <div class="preloader">
         <figure><img src="{{ asset('img/coralclean/favicon.png') }}" alt="CoralClean"></figure>
